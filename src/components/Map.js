678ab5d4a9f5c75273a227952b36c25dec0ca511
Steve Prager <stephenprager92@gmail.com>
2018-09-19 18:29:40 -0400

    Add map marker filtering

--- src/components/Map.js
+++ src/components/Map.js
@@ -21,6 +21,13 @@ class Map extends Component {
 		locations: PropTypes.array.isRequired
 	}
 
+    // State contains references to map and all  markers, we 
+    // will toggle visibility of these as needed
+	state = {
+		map: {},
+		markers: []
+	}
+
 	/* 
 	  RETRIEVE GOOGLE MAPS API WITHIN REACT - USED WHEN COMPONENT WILL AND DOES MOUNT
 
@@ -47,68 +54,91 @@ class Map extends Component {
 		        document.body.appendChild(script);
 	        });
 	    }
-
 	    // Return a promise for the Google Maps API
 	    return this.googleMapsPromise;
-  }
+	}
 
-  componentDidMount() {
+	componentDidMount() {
 
-    // Once the Google Maps API finishes loading, initialize the map at the Bethlehem city center
-    this.getMapsAPI().then((google) => {
-      const bethlehemCenter = {lat: 40.6139, lng: -75.3705};
-      const map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 14.5,
-        center: bethlehemCenter
-      });
+	    // Once the Google Maps API finishes loading, initialize the map at the Bethlehem city center
+        this.getMapsAPI().then((google) => {
+		    const bethlehemCenter = {lat: 40.6139, lng: -75.3705};
+		    const map = new google.maps.Map(document.getElementById('map'), {
+		    	zoom: 14.5,
+		    	center: bethlehemCenter
+	        });
 
-      // Create an info window for our markers
-      const infoWindow = new google.maps.InfoWindow();
+	        // Create an info window for our markers
+	        const infoWindow = new google.maps.InfoWindow();
 
-      // Add our neighborhood locations to the map as markers
-      for (let location of this.props.locations) {
-	      const marker = new google.maps.Marker({
-	          position: {lat: location.lat, lng: location.lng},
-	          title: location.title,
-	          map: map
-	      });
+	        // Add our neighborhood locations to the map as markers
+	        // Store our markers in the component's state as they are created
+	        let storedMarkers = []
+	        for (let location of this.props.locations) {
+			    const marker = new google.maps.Marker({
+			        position: {lat: location.lat, lng: location.lng},
+		            title: location.title,
+	                map: map
+	            });
 
-          // Add event listeners to each marker to assign an info window if clicked
-	      marker.addListener('click', function() {
-	          // Check to make sure the infowindow is not already opened on this marker
-	          if (infoWindow.marker !== marker) {
-			      infoWindow.marker = marker;
-		          infoWindow.setContent('<div id="info-window">' + marker.title + '</div>');
-			      infoWindow.open(map, marker);
-			      // Make sure the marker property is cleared if the infowindow is closed.
-		          infoWindow.addListener('closeclick', function() {
-				      infoWindow.marker = null;
-	              });
-		      }
-		  });
-		  // Add two more event listeners to toggle the (bounce) animation
-		  // of the marker on click. Animation then removed on mouseout
-          marker.addListener('click', function() {
-            this.setAnimation(google.maps.Animation.BOUNCE);
-          });
-          marker.addListener('mouseout', function() {
-            this.setAnimation(null);
-          });
-	  }   
-	});
-  }
+                // Add event listeners to each marker to assign an info window if clicked
+	            marker.addListener('click', function() {
+	                // Check to make sure the infowindow is not already opened on this marker
+	                if (infoWindow.marker !== marker) {
+			            infoWindow.marker = marker;
+		                infoWindow.setContent('<div id="info-window">' + marker.title + '</div>');
+			            infoWindow.open(map, marker);
+			            // Make sure the marker property is cleared if the infowindow is closed.
+		                infoWindow.addListener('closeclick', function() {
+					        infoWindow.marker = null;
+	                    });
+		            }
+		        });
+		        // Add two more event listeners to toggle the (bounce) animation
+		        // of the marker on click. Animation then removed on mouseout
+                marker.addListener('click', function() {
+	                this.setAnimation(google.maps.Animation.BOUNCE);
+                });
+                marker.addListener('mouseout', function() {
+	                this.setAnimation(null);
+                });
+                storedMarkers.push(marker)
+            }
+            // Set the map state variables of the component for future manipulation
+            this.setState({map: map, markers: storedMarkers})
+        });
+    }
 
-  // NEXT - WE NEED TO SET THE MARKER VISIBILITY TO NULL? - CHECK THE CLASS LESSON - AS COMPONENT UPDATES 
-  componentWillUpdate() {
+    /* 
+       When the component updates (recieves new filtered locations as props), set the 
+       visibility of the markers in the map accordingly
 
-  }
+       NOTE - Marker / location titles must be unique for this to work. This version of the 
+       app satisfies that but keep in mind going forward
+    */
+	componentDidUpdate() {
+		const { map, markers } = this.state
 
-  // Render the map (just a div)
-  render() {
-    return (
-        <div id="map"></div>
-    )
-  }
+	    // First, hide all markers from the map
+	  	for (let marker of markers) {
+		    marker.setMap(null)
+	  	}
+	    // Then, show the ones that are in the currently visible locations
+	  	for (let location of this.props.locations) {
+	  		for (let marker of markers) {
+	  			if (marker.title === location.title) {
+		  			marker.setMap(map)	
+	  			}
+	  		}
+	  	}
+	}
+
+	// Render the map (just a div)
+	render() {
+	    return (
+	        <div id="map"></div>
+	    )
+	}
 }
 
 export default Map
